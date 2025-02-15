91f80d770d1fa09a1624f6c468e426b746e9cfaa
Christopher Van Kampen <christopheryungvankampen@gmail.com>
2025-02-15 11:36:48 -0500

    work in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,117 +1,22 @@
-import { useEffect, useState } from 'react'
 import './App.css'
-import TodoList from './components/TodoList/TodoList'
-import AddTodoForm from './components/AddTodoForm'
+import TodoContainer from './components/TodoContainer'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
+import HomeContainer from './components/HomeContainer'
 
 function App() {
-  const [todoList, setTodoList] = useState([])
-
-  const [isLoading, setIsLoading] = useState(true)
-
-  const url = `https://api.airtable.com/v0/${import.meta.env.VITE_AIRTABLE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}?view=Grid%20view&sort[0][field]=title&sort[0][direction]=asc`
-
-  const fetchData = async () => {
-    const options = {
-      method: 'GET', headers: { Authorization: `Bearer ${import.meta.env.VITE_AIRTABLE_API_TOKEN}` }
-    }
-    try {
-      const response = await fetch(url, options)
-      if (!response.ok) {
-        throw new Error(`Error: ${response.status}`)
-      }
-      const data = await response.json()
-
-      const todos = data.records.map((todo) => {
-        const newTodo = {
-          title: todo.fields.title,
-          id: todo.id
-        }
-        return newTodo
-      })
-
-      todos.sort((a, b) => {
-        if (a.title < b.title) return 1
-        if (a.title > b.title) return -1
-        return 0
-      })
-
-      setTodoList(todos)
-      setIsLoading(false)
-
-    } catch (error) {
-      console.error()
-    }
-  }
-
-  useEffect(() => { fetchData() }, [])
-
-  useEffect(() => {
-    if (!isLoading) {
-      localStorage.setItem('savedTodoList', JSON.stringify(todoList))
-    }
-  }, [todoList])
-
-  const removeTodo = (id) => {
-    const itemList = todoList.filter((item) => {
-      return item.id !== id
-    })
-    setTodoList(itemList)
-  }
-
-  const addTodo = async (newTodo) => {
-    // const newList = [...todoList, newTodo] 
-    // setTodoList(newList)
-    const options = {
-      method: 'POST',
-      body: JSON.stringify(
-        {
-          fields: { 'title': newTodo.title }
-        }),
-      headers: {
-        Authorization: `Bearer ${import.meta.env.VITE_AIRTABLE_API_TOKEN}`,
-        'Content-Type': 'application/json',
-      }
-    }
-    try {
-      const response = await fetch(url, options)
-      if (!response.ok) {
-        throw new Error(`Error: ${response.status}`)
-      }
-      const data = await response.json()
-      console.log(data)
-
-      //create new list item data.fields.title
-      const newListItem = { title: data.fields.title, id: data.id }
-      //add item to current list with spread operators
-      const newList = [...todoList, newListItem]
-      //setTodoList
-      setTodoList(newList)
-
-    } catch (error) {
-      console.log(error)
-    }
-  }
 
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={
-          <>
-            <h1>Todo List</h1>
-            <AddTodoForm onAddTodo={addTodo} />
-            {isLoading ? <p>Loading List...</p> : <TodoList todoList={todoList} onRemoveTodo={removeTodo} />}
-          </>
+          <HomeContainer></HomeContainer>
         }>
         </Route>
-        <Route path="/new" element={
-          <h1>New Todo List</h1>
+        <Route path="/todos" element={
+          <TodoContainer tableName="Todo List" ></TodoContainer>
         }>
         </Route>
       </Routes>
-    </BrowserRouter>
-  )
+    </BrowserRouter >)
 }
-
 export default App
--- src/components/InputWithLabel.jsx
+++ src/components/InputWithLabel.jsx
@@ -9,7 +9,7 @@ const InputWithLabel = (props) => {
 
     useEffect(() => {
         inputRef.current.focus()
-    })
+    }, [])
 
     return (
         <>
